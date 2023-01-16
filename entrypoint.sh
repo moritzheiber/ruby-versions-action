687764813277ada72348771c2a7b5adbccc28924
Moritz Heiber <hello@heiber.im>
2023-01-16 11:40:27 +0100

    Fix checksums output not having the correct name

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,10 @@ VERSIONS="$(echo ${OUTPUT} | jq -Mjc '[.[].name]')"
 CHECKSUMS="$(echo ${OUTPUT} | jq -Mjc '[.[] | .["ruby"] = .name | .["ruby_checksum"] = .sha256 | del(.name,.url,.sha256)]')"
 export VERSIONS CHECKSUMS
 
+echo "Versions: ${VERSIONS}"
+echo "Checksums: ${CHECKSUMS}"
+
 if [ -n "${GITHUB_OUTPUT}" ] ; then
-    echo "checksum=${CHECKSUMS}" >> "${GITHUB_OUTPUT}"
+    echo "checksums=${CHECKSUMS}" >> "${GITHUB_OUTPUT}"
     echo "versions=${VERSIONS}" >> "${GITHUB_OUTPUT}"
 fi
